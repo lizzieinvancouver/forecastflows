6dea108d9b1f780babcd5cb6afa61bc158563b42
Elizabeth M Wolkovich <lizziemetier@gmail.com>
2025-07-22 10:13:07 -0700

    More efforts, still annoyed

--- figures/retrodic/simulations_speciesonlyintslope.R
+++ figures/retrodic/simulations_speciesonlyintslope.R
@@ -12,7 +12,8 @@ require(phytools)
 require(ggplot2)
 require(rstan)
 
-set.seed(1111)
+# set.seed(1111)
+set.seed(777)
 
 nspecies = 40
 # phytree <- pbtree(n=nspecies, nsim=1, b=1, complete=FALSE, scale=1)
@@ -20,7 +21,7 @@ nspecies = 40
 # tree <- pbtree(n = nspecies, scale = 1)
 # tree[["edge.length"]][c(1,10)] <- tree[["edge.length"]][c(1,10)]*2
 # phytree <- force.ultrametric(tree)
-phytree <- rcoal(n = nspecies, scale = 1)
+# phytree <- rcoal(n = nspecies, scale = 1)
 phytree <- rcoal(n = nspecies, scale = 1)
 plot(phytree)
 
@@ -28,7 +29,9 @@ plot(phytree)
 # ------------- #
 # Simulate data #
 # ------------- #
-whichspp <- c(8,15,17,21,28,32,35)
+# whichspp <- c(8,15,17,21,28,32,35) # seed(1111) with second trees
+
+whichspp <- c(4, 15:17,20, 25)
 
 nspecies <- phytree[["Nnode"]]+1 # no. of species
 nobs_perspecies <- round(runif(nspecies, 5, 15)) # no. of different observations per population
@@ -79,6 +82,7 @@ ggplot(data = data.frame(sp = speciesnum, mu_beta_sp_wphylogeny)) +
 ## simulate counts, here with the WITH phylogenetic structure
 yphylo <- c()
 sigma_obs <- 0.5
+
 for(p in 1:nspecies){
   nobs_p <- nobs_perspecies[p]
   years_p <- years[spid_perobs == p] 
@@ -96,6 +100,16 @@ datasim <- data.frame(
 
 datasim$logyphylo <- log(datasim$yphylo)
 
+dropspp <- c(1:20)
+
+datasimdrop <- data.frame(
+  yphylo[which(!spid_perobs %in% dropspp)],
+  year = years[which(!spid_perobs %in% dropspp)] - 1980,
+  species = as.character(spid_perobs)[which(!spid_perobs %in% dropspp)]
+)
+
+datasimdrop$logyphylo <- log(datasim$yphylo)[which(!spid_perobs %in% dropspp)]
+
 # -------------------------------------------------- #
 # Fit the model against data without phylo structure #
 # -------------------------------------------------- #
@@ -108,6 +122,17 @@ data <- list(
   year = years,
   spid = spid_perobs
 )
+
+if(FALSE){
+data <- list(
+  N = nrow(datasimdrop),
+  Nsp = length(unique(datasimdrop$species)),
+  y = datasimdrop$logyphylo,
+  year = datasimdrop$year,
+  spid = rep(1:nspecies, times = nobs_perspecies[which(!nobs_perspecies %in% dropspp)])
+)
+}
+
 fit <- sampling(object = m, data = data,  
                 iter=2024, warmup=1000, cores = 4, seed = 20012029)
 
@@ -137,3 +162,20 @@ logy_pred <- unlist(extract(fit, pars = 'logy_pred'))
 ggplot() +
   geom_density(aes(x = log(data$y)), alpha = 0.2, color = '#009d6c') +
   geom_density(aes(x = logy_pred), alpha = 0.2)
+
+par(mfrow=c(1,2))
+hist(log(data$y), n=20)
+hist(logy_pred, n=20)
+
+
+datame <- data.frame(spid = spid_perobs, y =yphylo)
+
+datame$whichspp <- NA
+datame$whichspp[which(datame$spid %in% whichspp)] <- "special"
+datame$whichspp[which(!datame$spid %in% whichspp)] <- "notspecial"
+
+ggplot() +
+  geom_histogram(aes(x = log(datame$y), color=datame$whichspp), alpha = 0.2)
+
+test <- datame[which(log(datame$y) < 10),]
+unique(test$spid)
