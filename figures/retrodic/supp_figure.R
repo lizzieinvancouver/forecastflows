155f2cb2923f7072d63e4234699c4c8993ed5982
vvandermeersch <victor.vandermeersch@cefe.cnrs.fr>
2025-09-25 18:33:36 -0700

    add missing script for Lizzie---since she loves so much working on this!

--- figures/retrodic/supp_figure.R
+++ figures/retrodic/supp_figure.R
@@ -28,7 +28,8 @@ beta_root <- 1.5
 lambda_alpha <- 0.99
 lambda_beta  <- 0.99
 
-s <- 3804643
+# s <- 38046488
+s <- 380977
 set.seed(s)
 
 {
@@ -38,8 +39,8 @@ set.seed(s)
   corrmat_alpha <- lambda_alpha * corrmat + (1 - lambda_alpha) * diag(nrow(corrmat))
   corrmat_beta  <- lambda_beta * corrmat + (1 - lambda_beta) * diag(nrow(corrmat))
   
-  sigma_y <- 0.1
-  sigma_alpha <- 0.3
+  sigma_y <- 2
+  sigma_alpha <- 1.5
   sigma_beta <- 1.5
   
   alpha <- MASS::mvrnorm(1, mu = rep(alpha_root, nspecies), Sigma = sigma_alpha^2 * corrmat_alpha)
@@ -48,8 +49,7 @@ set.seed(s)
   datasim <- data.frame()
   spid <- c()
   for(i in 1:nspecies){
-    # nperspecies <- ifelse(i == 6, 100, 5)
-    nperspecies <- 5
+    nperspecies <- ifelse(i %in% c(1:3), 20, 3)
     x <- rnorm(nperspecies, mean = 0, sd = 3)
     y <- alpha[i] + beta[i] * x + rnorm(nperspecies, mean = 0, sd = sigma_y)
     datasim <- rbind(
@@ -86,7 +86,7 @@ set.seed(s)
   
   par(mfrow=c(1, 2))
   yrep <- rstan::extract(fit, pars = "y_pred")$y_pred 
-  resid_matrix <- sweep(yrep, 2, data$y, FUN = "-")  
+  resid_matrix <- sweep(yrep, 2, data$y, FUN = "-")
   # resid_mean <- colMeans(resid_matrix) 
   residnophylo <- data.frame(mod = "Standard", sp = 1:data$Nsp,  t(sapply(1:data$Nsp, function(sp) quantile(as.numeric(resid_matrix[,data$spid==sp]), probs = c(0.25, 0.5, 0.75)))),
                              Xmean = sapply(1:data$Nsp, function(sp) mean(as.numeric(resid_matrix[,data$spid==sp]))))
@@ -99,66 +99,99 @@ set.seed(s)
   
   
   # Below code for plots from https://www.joelnitta.com/posts/2021-06-02_color-scheme-anc-states/
+  minlim <- min(min(residnophylo$X50.), min(residphylo$X50.))
+  maxlim <- max(max(residnophylo$X50.), max(residphylo$X50.))
+  # maxlim <- 1 
+  
   resid <- residnophylo$X50.
   names(resid) <- tree$tip.label
-  fit <- phytools::fastAnc(tree, resid, vars = TRUE, CI = TRUE)
+  fit.fastAnc <- phytools::fastAnc(tree, resid, vars = TRUE, CI = TRUE)
   td <- data.frame(
     node = ggtree::nodeid(tree, names(resid)),
     trait = resid)
-  nd <- data.frame(node = names(fit$ace), trait = fit$ace)
+  nd <- data.frame(node = names(fit.fastAnc$ace), trait = fit.fastAnc$ace)
   d <- rbind(td, nd)
   d$node <- as.numeric(d$node)
   treemod <- dplyr::full_join(tree, d, by = 'node')
   plottree_nophylo <- ggtree::ggtree(
     treemod, aes(color = trait), 
     ladderize = FALSE, continuous = "color", size = 1) +
-    scale_colour_viridis_c(limits = c(-0.041,0.03)) + 
+    scale_colour_viridis_c(limits = c(minlim,maxlim)) + 
     theme(
       legend.position = c(0.4, .05),
       legend.direction = 'horizontal',
       legend.text = element_text(size = 9),
       legend.title = element_blank(),
-      legend.key.width =  unit(2,"line"),
+      legend.key.width =  unit(1.5,"line"),
       legend.key.height =  unit(0.5,"line")
     ) 
   
   
   resid <- residphylo$X50.
   names(resid) <- tree$tip.label
-  fit <- phytools::fastAnc(tree, resid, vars = TRUE, CI = TRUE)
+  fit.fastAnc <- phytools::fastAnc(tree, resid, vars = TRUE, CI = TRUE)
   td <- data.frame(
     node = ggtree::nodeid(tree, names(resid)),
     trait = resid)
-  nd <- data.frame(node = names(fit$ace), trait = fit$ace)
+  nd <- data.frame(node = names(fit.fastAnc$ace), trait = fit.fastAnc$ace)
   d <- rbind(td, nd)
   d$node <- as.numeric(d$node)
   treemod <- dplyr::full_join(tree, d, by = 'node')
   plottree_phylo <- ggtree::ggtree(
     treemod, aes(color = trait), 
     ladderize = FALSE, continuous = "color", size = 1) +
-    scale_colour_viridis_c(limits = c(-0.041,0.03)) + 
+    scale_colour_viridis_c(limits = c(minlim,maxlim))  + 
     theme(
       legend.position = 'none',
       legend.direction = 'horizontal',
       legend.text = element_text(size = 9),
       legend.title = element_blank(),
-      legend.key.width =  unit(2,"line"),
+      legend.key.width =  unit(1,"line"),
       legend.key.height =  unit(0.1,"line")
     ) +  scale_x_reverse()
   
-  plot_bias <- ggplot(data = rbind(residnophylo, residphylo)) +
-    geom_vline(aes(xintercept = 0), linetype = 'dashed', color = 'grey40') + 
-    geom_pointrange(aes(y = sp, xmin = X25., xmax = X75.,x = X50., group = mod, color = mod),
-                    position = position_dodge(width = 0.5)) +
+  # plot_bias <- ggplot(data = rbind(residnophylo, residphylo)) +
+  #   geom_vline(aes(xintercept = 0), linetype = 'dashed', color = 'grey40') + 
+  #   geom_pointrange(aes(y = sp, xmin = X25., xmax = X75.,x = X50., group = mod, color = mod),
+  #                   position = position_dodge(width = 0.5), size = 0.3) +
+  #   theme_classic() +
+  #   theme(axis.line.y = element_blank(), axis.ticks.y = element_blank(),
+  #         axis.text.y = element_blank(), axis.title.y = element_blank(),
+  #         legend.position = 'none') +
+  #   labs(x = 'Residuals (ypred - yobs)')
+  
+  #  plottree_nophylo + plot_bias + plottree_phylo
+  
+  summ_fitnophylo <- data.frame(summary(fit)[["summary"]])
+  summ_fitphylo <- data.frame(summary(fitphylo)[["summary"]])
+  
+  slopesnophylo <- data.frame(
+    slp_fit = summ_fitnophylo[paste0("mu_beta_sp[",1:nspecies,"]"), c('X50.')],
+    slp_fit.lcl = summ_fitnophylo[paste0("mu_beta_sp[",1:nspecies,"]"), c('X2.5.')],
+    slp_fit.ucl = summ_fitnophylo[paste0("mu_beta_sp[",1:nspecies,"]"), c('X97.5.')],
+    sp = 1:nspecies,
+    mod = 'Standard'
+  )
+  
+  slopesphylo <- data.frame(
+    slp_fit = summ_fitphylo[paste0("mu_beta_sp[",1:nspecies,"]"), c('X50.')],
+    slp_fit.lcl = summ_fitphylo[paste0("mu_beta_sp[",1:nspecies,"]"), c('X2.5.')],
+    slp_fit.ucl = summ_fitphylo[paste0("mu_beta_sp[",1:nspecies,"]"), c('X97.5.')],
+    sp = 1:nspecies,
+    mod = 'Phylogenetic\nstructure'
+  )
+  
+  plot_slopes <- ggplot(data = rbind(slopesnophylo, slopesphylo)) +
+    # geom_vline(aes(xintercept = 0), linetype = 'dashed', color = 'grey40') +
+    geom_pointrange(aes(y = sp, xmin = slp_fit.lcl, xmax = slp_fit.ucl,x = slp_fit, group = mod, color = mod),
+                    position = position_dodge(width = 0.5), size = 0.3) +
     theme_classic() +
     theme(axis.line.y = element_blank(), axis.ticks.y = element_blank(),
           axis.text.y = element_blank(), axis.title.y = element_blank(),
           legend.position = 'none') +
-    labs(x = 'Residuals (ypred - yobs)')
-  
-  plottree_nophylo + plot_bias + plottree_phylo
-  
-  
+    labs(x = 'Slopes')
+
+  plottree_nophylo + plot_slopes + plottree_phylo
 }
 
 
